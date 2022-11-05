156056146422651388641fa2b4836f2fb5aa1fa4
Nghia <nghiamai11111@gmail.com>
2022-11-05 15:16:28 +0700

    My project

---
+++ m.py
@@ -0,0 +1,38 @@
+# a = open(r'D:\exercise_vscode\pythonProject\ghifile.txt', 'r+')
+# for line in a:
+#     print(line)
+#
+#     while True:
+#         self.Name = input('Nhập tên sản phẩm: ')
+#         if 3 <= len(self.Name) <= 5 and self.Name[0:2] == "TN":
+#             infor['Name'] = self.Name
+#             self.Name = True
+#             break
+#         else:
+#             print("Yêu cầu nhập lại!")
+#     while True:
+#         self.country = input("Nhập nước sản xuất: ")
+#         if self.country == 'Hàn Quốc' or self.country == "Nhật Bản":
+#             infor['country'] = self.country
+#             self.country = True
+#             break
+#         else:
+#             print('Yêu cầu nhập lại!')
+#     self.cost = int(input("Nhập giá sản phẩm: "))
+#     infor['cost'] = self.cost
+#     while True:
+#         self.quantity = int(input("Nhập số lượng sản phẩm: "))
+#         if self.quantity >= 3:
+#             infor['quantity'] = self.quantity
+#             self.quantity = True
+#             break
+#         else:
+#             print("Yêu cầu nhập lại")
+#     self.data.append(infor)
+# for i in range(len(self.data)):
+#     if self.ID == self.data[i]['ID']:
+#         print('Yêu cầu nhập lại')
+#         self.ID = input("Nhập lại ID: ")
+# for i in self.data:
+#     print(i)
+help(eval())
