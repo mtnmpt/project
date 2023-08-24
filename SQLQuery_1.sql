8af4596c37865b6a1f2e17c22b054cbd6b4fb34a
nghia <nghiamai11111@gmail.com>
2023-08-24 09:42:52 +0700

    sql

---
+++ SQLQuery_1.sql
@@ -0,0 +1,22 @@
+-- Create a new stored procedure called 'StoredProcedureName' in schema 'dbo'
+-- Drop the stored procedure if it already exists
+-- IF EXISTS (
+-- SELECT *
+--     FROM INFORMATION_SCHEMA.ROUTINES
+-- WHERE SPECIFIC_SCHEMA = N'dbo'
+--     AND SPECIFIC_NAME = N'StoredProcedureName'
+--     AND ROUTINE_TYPE = N'PROCEDURE'
+-- )
+-- DROP PROCEDURE dbo.StoredProcedureName
+-- GO
+-- Create the stored procedure in the specified schema
+ALTER PROCEDURE SP_item1_GetList
+@ItemName         NVARCHAR(50) 
+AS
+BEGIN
+    SELECT * 
+    FROM   item1
+    WHERE  ( ISNULL(@ItemName,'') = '' OR ItemName = @ItemName )
+END
+GO
+-- example to execute the stored procedure we just created
